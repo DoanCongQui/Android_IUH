9f4f10dfda36a75bf09423f10b62c8cf5ea47835
DoanCongQui <congt4644@gmail.com>
2025-01-27 12:02:27 +0700

    Intent

--- Bai2_button/app/src/main/java/com/example/bai2/MainActivity.java
+++ Bai2_button/app/src/main/java/com/example/bai2/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,11 +17,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         btnBack = findViewById(R.id.button);
         btnBack.setOnClickListener(new View.OnClickListener() {
+
+            /*
+            * ===================================================
+            *                       Intent
+            * ===================================================
+            * - ACTION_VIEW |
+            * - ACTION_DIAL |  Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+            * - ACTION_EDIT |
+            * - ACTION_PICK
+            * */
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-                startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                startActivityForResult(intent, 1234);
             }
         });
+
     }
 }
